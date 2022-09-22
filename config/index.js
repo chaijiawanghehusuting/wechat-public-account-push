ec448ebacc9a431faf6b6c3d8e00ec04187a3226
chaijiawanghehusuting <114060378+chaijiawanghehusuting@users.noreply.github.com>
2022-09-23 00:57:46 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const config = {
       // 想要发送的人的名字
       name: "胡宝宝",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "owQO854OIseyou9pVoSqIeSlw03Q,owQO854Q97P51cE-QidRIVV5NmpY",
+      id: "owQO854Q97P51cE-QidRIVV5NmpY",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "DpPPXZ87TTQ3h4ys34eZKA0_-s_j_bw8ScI93JiYu68",
       // 所在省份
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "",
+  CALLBACK_TEMPLATE_ID: "DpPPXZ87TTQ3h4ys34eZKA0_-s_j_bw8ScI93JiYu68",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -147,7 +147,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "owQO854OIseyou9pVoSqIeSlw03Q",
     },
     // 你可以不断按格式往下增加
     // ...
