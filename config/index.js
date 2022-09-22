512d6ad04833b49f651f92150dd680738e1cac55
chaijiawanghehusuting <114060378+chaijiawanghehusuting@users.noreply.github.com>
2022-09-23 01:22:44 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -64,7 +64,7 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "owQO854Q97P51cE-QidRIVV5NmpY",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "DpPPXZ87TTQ3h4ys34eZKA0_-s_j_bw8ScI93JiYu68",
+      useTemplateId: "EJWvyOvOwNglTg0jAfmNv4LYrf7txfu0UvyzHOvWczg",
       // 所在省份
       province: "浙江",
       // 所在城市
@@ -87,11 +87,11 @@ export const config = {
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        {"keyword": "love_day", date: ""},
+        {"keyword": "love_day", date: "2021-04-05"},
         // 结婚纪念日
-        {"keyword": "marry_day", date: "2022-09-09"},
+        {"keyword": "marry_day", date: ""},
         // 退伍日
-        {"keyword": "ex_day", date: "2022-09-10"},
+        {"keyword": "ex_day", date: ""},
       ]
     },
     {
@@ -139,7 +139,7 @@ export const config = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "DpPPXZ87TTQ3h4ys34eZKA0_-s_j_bw8ScI93JiYu68",
+  CALLBACK_TEMPLATE_ID: "EJWvyOvOwNglTg0jAfmNv4LYrf7txfu0UvyzHOvWczg",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
